58651162e568c38c2aa0aa2988213b4c270831d6
Elkinrodriguez03 <elkinrodriguez03@gmail.com>
2022-08-30 09:21:23 -0500

    Simplifying and organizing code

--- js/code.js
+++ js/code.js
@@ -1,47 +1,126 @@
+const sectionSelectAttack = document.getElementById('select-attack')
+const sectionNewFight = document.getElementById('reset')
+const playerFighterButton = document.getElementById('fighter-button')
+const clawsButton = document.getElementById('claws-button')
+const shotgunButton = document.getElementById('shotgun-button')
+const fangsButton = document.getElementById('fangs-button')
+const newFightButton = document.getElementById('reset-button')
+sectionNewFight.style.display = 'none'
+
+const sectionFighterSelect = document.getElementById('select-fighter')
+
+const spanPlayerFighter = document.getElementById('player-fighter')
+const spanEnemyFighter = document.getElementById('enemy-fighter')
+
+const spanPlayerLifes = document.getElementById('player-lifes')
+const spanEnemyLifes = document.getElementById('enemy-lifes')
+
+const sectionMessages = document.getElementById('messages')
+const playerAttacks = document.getElementById('player-attacks')
+const enemyAttacks = document.getElementById('enemy-attacks')
+const cardsContainer = document.getElementById('cards-container')
+
+let monsterFighters = []
 let playerAttack
 let enemyAttack
+let monsterFighterOptions
 let playerLifes = 3
 let enemyLifes = 3
+let inputHuman
+let inputVampire
+let inputWerewolf
+
+class MonsterFighter {
+    constructor(name, picture, life) {
+        this.name = name
+        this.picture = picture
+        this.life = life
+        this.attacks = []
+    }
+}
+
+let human = new MonsterFighter('Human', '/assets/Old_MMA_fighter.png', 5)
+
+let vampire = new MonsterFighter('Vampire', '/assets/vampire_fighter.png', 5)
+
+let werewolf = new MonsterFighter('Werewolf', '/assets/werewolf_fighter.png', 5)
+
+human.attacks.push(
+    { name: 'Glock G19', id: 'shotgun-button' },
+    { name: 'M&P15 Rifle', id: 'shotgun-button' },
+    { name: 'Mossberg 500', id: 'shotgun-button' },
+    { name: 'Knife', id: 'claws-button' },
+    { name: 'Axe', id: 'claws-button' },
+)
+
+vampire.attacks.push(
+    { name: 'Poison Fangs', id: 'fangs-button' },
+    { name: 'Spit Acid', id: 'fangs-button' },
+    { name: 'Mind Control', id: 'fangs-button' },
+    { name: 'Knife', id: 'claws-button' },
+    { name: 'Axe', id: 'claws-button' },
+)
+
+werewolf.attacks.push(
+    { name: 'Iron Claws', id: 'claws-button' },
+    { name: 'Super Bite', id: 'claws-button' },
+    { name: 'Spit Acid', id: 'fangs-button' },
+    { name: 'Knife', id: 'claws-button' },
+    { name: 'Axe', id: 'claws-button' },
+)
+
+monsterFighters.push(human, vampire, werewolf)
 
 function startGame() {
-    let sectionSelectAttack = document.getElementById('select-attack')
-    sectionSelectAttack.style.display = 'none'
-
-    let sectionNewFight = document.getElementById('reset')
-    sectionNewFight.style.display = 'none'
     
-    let playerFighterButton = document.getElementById('fighter-button')
+    sectionSelectAttack.style.display = 'none'
+    
+    monsterFighters.forEach((monsterFighter) => {
+        monsterFighterOptions = `
+        <input type="radio" name="fighter" id=${monsterFighter.name} />
+        <label class="fighter-card" for=${monsterFighter.name}>
+            <p>${monsterFighter.name}</p>
+            <img src=${monsterFighter.picture} alt=${monsterFighter.name}>
+        </label>
+        `
+    cardsContainer.innerHTML += monsterFighterOptions
+
+        inputHuman = document.getElementById('Human')
+        inputVampire = document.getElementById('Vampire')
+        inputWerewolf = document.getElementById('Werewolf')
+    })
+    
+    
     playerFighterButton.addEventListener('click', selectPlayerFighter   )
-
-    let shotgunButton = document.getElementById('shotgun-button')
+    
+    
     shotgunButton.addEventListener('click', shotgunAttack)
-    let fangsButton = document.getElementById('fangs-button')
+    
     fangsButton.addEventListener('click', fangsAttack)
-    let clawsButton = document.getElementById('claws-button')
+    
     clawsButton.addEventListener('click', clawsAttack)
-
-    let newFightButton = document.getElementById('reset-button')
+    
+    
     newFightButton.addEventListener('click', resetGame)
 }
 
 function selectPlayerFighter() {
-    let sectionFighterSelect = document.getElementById('select-fighter')
+    
     sectionFighterSelect.style.display = 'none'
     
-    let sectionAttackSelect = document.getElementById('select-attack')
-    sectionAttackSelect.style.display = 'flex'
     
-    let inputHuman = document.getElementById('human')
-    let inputVampire = document.getElementById('vampire')
-    let inputWerewolf = document.getElementById('werewolf')
-    let spanPlayerFighter = document.getElementById('player-fighter')
+    sectionSelectAttack.style.display = 'flex'
+    
+    
+    
+    
     
     if (inputHuman.checked) {
-        spanPlayerFighter.innerHTML = 'Human'
+        spanPlayerFighter.innerHTML = inputHuman.id
     } else if (inputVampire.checked) {
-        spanPlayerFighter.innerHTML = 'Vampire'
+        spanPlayerFighter.innerHTML = inputVampire.id
     } else if (inputWerewolf.checked) {
-        spanPlayerFighter.innerHTML = 'Werewolf'
+        spanPlayerFighter.innerHTML = inputWerewolf.id
     } else {
         alert('Choose a Fighter')
     }
@@ -51,7 +130,7 @@ function selectPlayerFighter() {
 
 function selectEnemyFighter() {
     let randomFighter = random(1,3)
-    let spanEnemyFighter = document.getElementById('enemy-fighter')
+    
 
     if (randomFighter == 1) {
         spanEnemyFighter.innerHTML = 'Human'
@@ -90,8 +169,7 @@ function randomEnemyAttack() {
 }
 
 function fight() {
-    let spanPlayerLifes = document.getElementById('player-lifes')
-    let spanEnemyLifes = document.getElementById('enemy-lifes')
+    
     
     if(enemyAttack == playerAttack) {
         newMessage("TIE")
@@ -125,35 +203,33 @@ function lifeCheck() {
 }
 
 function newMessage(result) {
-    let sectionMessages = document.getElementById('messages')
-    let playerAttacks = document.getElementById('player-attacks')
-    let enemyAttacks = document.getElementById('enemy-attacks')
+    
     
     let newPlayerAttack = document.createElement('p')
     let newEnemyAttack = document.createElement('p')
-
+    
     sectionMessages.innerHTML = result
     newPlayerAttack.innerHTML = playerAttack
     newEnemyAttack.innerHTML = enemyAttack
-
+    
     playerAttacks.appendChild(newPlayerAttack)
     enemyAttacks.appendChild(newEnemyAttack)
 }
 
 function newFinalMessage(finalResult) {
-    let sectionMessages = document.getElementById('messages')
+    
     
     sectionMessages.innerHTML = finalResult
-
-    let shotgunButton = document.getElementById('shotgun-button')
+    
+    
     shotgunButton.disabled = true
-    let fangsButton = document.getElementById('fangs-button')
+    
     fangsButton.disabled = true
-    let clawsButton = document.getElementById('claws-button')
+    
     clawsButton.disabled = true
-
-    let sectionNewFight = document.getElementById('reset')
+    
     sectionNewFight.style.display = 'block'
+    
 }
 
 function resetGame() {
