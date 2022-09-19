17cce846e0446683b325ddb460062557bd1948d4
Elkinrodriguez03 <elkinrodriguez03@gmail.com>
2022-09-19 03:47:03 -0500

    Still working on it

--- public/js/code.js
+++ public/js/code.js
@@ -157,7 +157,7 @@ function startGame() {
 }
 
 function joinGame() {
-    fetch(`${URL}join`)
+    fetch(`${URL}/join`)
         .then(function (res) {
             if (res.ok) {
                 res.text()
